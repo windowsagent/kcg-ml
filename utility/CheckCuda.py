639e74ea1ff9ef280becedf65ac614076325cf7b
ssss <sdevdariani9@gmail.com>
2023-03-16 11:00:05 +0400

    add CheckCuda

---
+++ utility/CheckCuda.py
@@ -0,0 +1,21 @@
+import torch
+
+def check_cuda():
+    """
+    Checks if the system has a CUDA-capable GPU.
+
+    Returns:
+        bool: True if a CUDA device is available, False otherwise.
+    """
+    if torch.cuda.is_available():
+        cuda_version = torch.version.cuda
+        cuda_string = f"CUDA version: {cuda_version}"
+        print(cuda_string)
+        return True
+    else:
+        print("Warning: CUDA is not available. Running on CPU.")
+        return False
+
+
+
+check_cuda()   
